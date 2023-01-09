0125c0cb7d58bb07fc6edfff63acb7510762746b
matheustenedini <matheusltenedini@gmail.com>
2023-01-09 14:30:33 -0300

    feat: add favicon with dark mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="assets/favicon.png"
+          media="(prefers-color-scheme: light)"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          href="assets/favicon-dark.png"
+          media="(prefers-color-scheme: dark)"
+          type="image/png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
