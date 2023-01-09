3b990933f1b06e4142ab2dac2f8d6a2364818f45
matheustenedini <matheusltenedini@gmail.com>
2023-01-08 21:30:29 -0300

    fix: add prettier rule to fix clrf problem and removed button component outside folder (mistake)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,39 @@
-import { Montserrat } from '@next/font/google';
 import Header from '../components/Header';
+import { FiUpload } from 'react-icons/fi';
+import Button from '../components/Button';
+import Search from '../components/Search';
+import Filter from '../components/Filter';
+import Sort from '../components/Sort';
+import PatientsList from '../components/PatientsList';
 
-const inter = Montserrat({ weight: ['400', '500', '600', '700'] });
 export default function Home() {
   return (
-    <div className={inter.className}>
+    <div>
       <Header />
+
+      <main className="w-full py-16 px-12">
+        <div className="flex items-center justify-between">
+          <h2 className="text-4xl font-semibold text-slate-900">Pacientes</h2>
+          {/* buttons */}
+        </div>
+        <div className="mt-6 flex justify-between">
+          <div className="flex gap-4">
+            <Search placeholder="Buscar paciente..."></Search>
+            <Filter />
+            <Sort />
+          </div>
+          <div className="flex gap-4">
+            {/* Buttons */}
+            <Button variant="secondary" onClick={() => console.log('aaaa')}>
+              <FiUpload className="text-sm" />
+              Exportar tabela
+            </Button>
+            <Button variant="primary">Add paciente</Button>
+          </div>
+        </div>
+
+        <PatientsList />
+      </main>
     </div>
   );
 }
