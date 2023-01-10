be80df7537034b9b4f5c66d8bebb1c65236832ac
matheustenedini <matheusltenedini@gmail.com>
2023-01-10 20:45:00 -0300

    feat: add components to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,25 @@
 import Header from '../components/Header';
-import { FiUpload } from 'react-icons/fi';
-import Button from '../components/Button';
-import Search from '../components/Search';
-import Filter from '../components/Filter';
-import Sort from '../components/Sort';
 import PatientsList from '../components/PatientsList';
+import Head from 'next/head';
+import ListFilters from '../components/ListFilters';
+import ListActions from '../components/ListActions';
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-full max-h-screen w-full flex-col overflow-y-auto">
+      <Head>
+        <title>ACME | Pacientes</title>
+      </Head>
+
       <Header />
 
-      <main className="w-full py-16 px-12">
-        <div className="flex items-center justify-between">
+      <main className="flex w-full flex-1 flex-col overflow-hidden px-12 pt-16 pb-8">
+        <div>
           <h2 className="text-4xl font-semibold text-slate-900">Pacientes</h2>
-          {/* buttons */}
         </div>
         <div className="mt-6 flex justify-between">
-          <div className="flex gap-4">
-            <Search placeholder="Buscar paciente..."></Search>
-            <Filter />
-            <Sort />
-          </div>
-          <div className="flex gap-4">
-            {/* Buttons */}
-            <Button variant="secondary" onClick={() => console.log('aaaa')}>
-              <FiUpload className="text-sm" />
-              Exportar tabela
-            </Button>
-            <Button variant="primary">Add paciente</Button>
-          </div>
+          <ListFilters />
+          <ListActions />
         </div>
 
         <PatientsList />
